73c5115ebbe18ed8ce1647f559bc4f73564b92b3
dandyson2 <dandysonvictor@yahoo.com>
2023-11-16 23:09:23 +0000

    Sky is the limit, let's bring that limit higher, Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,3 @@
-# This Puppet code increases the ULIMIT of the default file for an Nginx server 
-# and restarts the Nginx service to handle more traffic.
-
 # Increase the ULIMIT of the default file for Nginx
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
