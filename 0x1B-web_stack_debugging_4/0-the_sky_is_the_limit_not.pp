71d5abae560a34eefd4371c5fc72ddf53455bb24
dandyson2 <dandysonvictor@yahoo.com>
2023-11-16 23:29:05 +0000

    Sky is the limit, let's bring that limit higher, Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,13 @@
+# Puppet manifest to optimize Nginx server for increased traffic handling
+
 # Increase the ULIMIT of the default file for Nginx
-exec { 'fix--for-nginx':
+exec { 'fix--for-nginx-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart Nginx to apply the changes
 exec { 'nginx-restart':
-  command => 'nginx restart',
+  command => 'service nginx restart',
   path    => '/etc/init.d/'
 }
