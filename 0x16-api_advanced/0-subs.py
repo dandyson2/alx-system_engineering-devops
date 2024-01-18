8ca8ada1e263c4bfc81823c9663be8a4e69fae2c
dandyson2 <dandysonvictor@yahoo.com>
2024-01-18 00:52:59 -0800

    How many subs, Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
 """
-This script contains the number_of_subscribers function
+Contains the number_of_subscribers function
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    This function returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit for which you want to get t
-
-    Returns:
-        int: The number of subscribers for the subreddit, or 0 if there was
-    """
-    # Check if the subreddit is a valid string
-    if subreddit is None or not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-    # Send a GET request to Reddit API to retrieve subreddit information
-    response = requests.get(f'http://www.reddit.com/r/{subreddit}/about.json',
-                            headers={'User-Agent': '0x16-api_advanced:\
-                                project:v1.0.0 (by /u/firdaus_cartoon_jr)'})
-
-    # Check if the request was successful
-    if response.status_code != 200:
-        return 0
-
-    # Parse the JSON response and extract the number of subscribers
-    data = response.json()
-    subscribers = data.get("data", {}).get("subscribers", 0)
-
-    return subscribers
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
